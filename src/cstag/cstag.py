467639cd06ce28fa617a9b2273c1805118207ae6
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-13 07:02:53 +0900

    Update cstag.py

--- src/cstag/cstag.py
+++ src/cstag/cstag.py
@@ -1,9 +1,22 @@
-def shorten():
+def shorten(sam: list) -> list:
     """Convert long format of cs tag into short format
+
+    Args:
+        sam (list): List of SAM (Sequence Alignment/Map Format) including a cs tag in **long** form
+
+    Returns:
+        sam (list): List of SAM (Sequence Alignment/Map Format) including a cs tag in **short** form
+
+    Example:
     """
     print("Hello!!")
 
-def lengthen():
+def lengthen(sam: list) -> list:
     """Convert short format of cs tag into long format
+    Args:
+        sam (list): List of SAM (Sequence Alignment/Map Format) including a cs tag in **short** form
+
+    Returns:
+        sam (list): List of SAM (Sequence Alignment/Map Format) including a cs tag in **long** form
     """
     print("こんにちは")
