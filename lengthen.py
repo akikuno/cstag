888c340d3695bf85f3d2cc0061623fd1d6f61a7e
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-14 05:05:52 +0900

    Update lengthen.py

--- lengthen.py
+++ lengthen.py
@@ -11,7 +11,9 @@ header = [s.split("\t") for s in sam  if re.search(r"^@", s)]
 
 contents = [s for s in sam  if not re.search(r"^@", s)]
 
+# ここからfor-loop
 content = contents[0]
+# for content in contents:
 
 if not "cs:Z" in content:
     try:
@@ -20,4 +22,9 @@ if not "cs:Z" in content:
         print(e)
         sys.exit(1)
 
+names = ["QNAME", "FLAG", "RNAME", "POS", "MAPQ", "CIGAR", "RNEXT", "PNEXT", "TLEN", "SEQ", "QUAL"]
 
+tmp = {n:c for n,c in zip(names, content.split("\t"))}
+tmp.update({"CS": c for c in content.split("\t") if re.search(r"^cs:Z",c)})
+
+tmp
