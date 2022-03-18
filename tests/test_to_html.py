836372a16cfbb9049438e403ee690416ef808a02
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-18 14:55:38 +0900

    Close #4

---
+++ tests/test_to_html.py
@@ -0,0 +1,11 @@
+import re
+import os
+import filecmp
+from src.cstag import to_html
+
+cs = "cs:Z:=AC+GGG=T-ACGT*at~gt10cg=GNNN"
+output = os.path.join("/tmp", "report")
+description = "Example"
+to_html(cs, output, description)
+
+assert filecmp.cmp(output + ".html", os.path.join("tests", "data", "to_html", "report.html"))
