62c3f3851e67ce1d103edd95f173c254089fb684
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-19 16:40:57 +0900

    format by black

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/akikuno/cstag",
     packages=setuptools.find_packages(
-        where='src',
+        where="src",
     ),
     package_dir={"": "src"},
     classifiers=[
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
