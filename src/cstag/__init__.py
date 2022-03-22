ff590212fc705ddc96bc4799c12902c92ae71f2e
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-22 13:56:58 +0900

    update names of subclass

--- src/cstag/__init__.py
+++ src/cstag/__init__.py
@@ -2,7 +2,19 @@
 .. include:: ../../README.md
 """
 
-from .length import shorten, lengthen
-from .cons import consensus
-from .convert import to_html
+from .shorten import shorten
+from .lengthen import lengthen
+from .consensus import consensus
+from .to_html import to_html
 from .mask import mask
+
+
+# import os
+# import glob
+
+# dir_init = glob.glob(os.path.join("src","*","__init__.py"))[0]
+# dir_init = os.path.dirname(dir_init)
+
+# modules = [f for f in os.listdir(dir_init) if not f.startswith("__")]
+# __all__ = [module.strip(".py") for module in modules]
+# print(__all__)
---
+++ src/cstag/shorten.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+
+def shorten(CSTAG: str) -> str:
+    """Convert long format of cs tag into short format
+    Args:
+        CSTAG (str): cs tag in the **long** format
+    Return:
+        str: cs tag in the **short** format
+    Example:
+        >>> import cstag
+        >>> cs = "cs:Z:=ACGT*ag=CGT"
+        >>> cstag.shorten(cs)
+        cs:Z::4*ag:3
+    """
+    cstags = re.split(r"([-+*~=])", CSTAG.replace("cs:Z:", ""))[1:]
+    cstags = [i + j for i, j in zip(cstags[0::2], cstags[1::2])]
+
+    csshort = []
+    for cs in cstags:
+        if cs[0] == "=":
+            csshort.append(":" + str(len(cs) - 1))
+            continue
+        csshort.append(cs)
+
+    return "cs:Z:" + "".join(csshort)
+
