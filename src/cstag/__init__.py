deccfeca0fc6bb8ae274cf06484ff3cb7bfb55fc
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-22 14:02:53 +0900

    refactoring

--- src/cstag/__init__.py
+++ src/cstag/__init__.py
@@ -9,12 +9,3 @@ from .to_html import to_html
 from .mask import mask
 
 
-# import os
-# import glob
-
-# dir_init = glob.glob(os.path.join("src","*","__init__.py"))[0]
-# dir_init = os.path.dirname(dir_init)
-
-# modules = [f for f in os.listdir(dir_init) if not f.startswith("__")]
-# __all__ = [module.strip(".py") for module in modules]
-# print(__all__)
