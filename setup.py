941ef87ce28f0ecfc7087cef9cc82b692a775b6f
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-31 13:46:11 +0900

    v0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="cstag",
-    version="0.2.1",
+    version="0.2.2",
     author="Akihiro Kuno",
     author_email="akuno@md.tsukuba.ac.jp",
     description="Python module to manipulate the minimap2's CS tag",
