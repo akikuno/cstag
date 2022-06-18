44e57c2543274b94346b403c45ded50f7622e0d6
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-06-18 15:26:50 +0900

    debug: add name method to call output file

--- tests/test_to_html.py
+++ tests/test_to_html.py
@@ -6,7 +6,7 @@ from src.cstag import to_html
 
 def test_html():
     cs = "cs:Z:=AC+GGG=T-ACGT*at~gt10cg=GNNN"
-    output = tempfile.NamedTemporaryFile()
+    output = tempfile.NamedTemporaryFile().name
     description = "Example"
     to_html(cs, output, description)
 
