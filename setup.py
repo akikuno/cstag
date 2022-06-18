38439379198e663da9c13db80c2ccc316c38c63f
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-06-18 14:54:02 +0900

    revert to support from Py 3.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="cstag",
-    version="0.2.2",
+    version="0.2.3",
     author="Akihiro Kuno",
     author_email="akuno@md.tsukuba.ac.jp",
     description="Python module to manipulate the minimap2's CS tag",
@@ -15,7 +15,6 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src",),
     package_dir={"": "src"},
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -23,5 +22,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
