406bfc702742a2866667e7e1dd0c2640b02e0940
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-09-17 05:20:16 +0900

    🗑️remove unused test data

--- tests/data/substitution/generate_sub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ref=tests/data/random_100bp.fa
-que=tests/data/substitution/sub.fq
-
-# SAM
-minimap2 -ax map-ont --cs "$ref" "$que" >tests/data/substitution/sub_cs.sam
-minimap2 -ax map-ont --cs=long "$ref" "$que" >tests/data/substitution/sub_cslong.sam
