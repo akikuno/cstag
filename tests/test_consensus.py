ee6043c9a00799c1362188f7e9f6ed327f0b9b23
akikuno <akuno@md.tsukuba.ac.jp>
2023-09-03 10:25:57 +0900

    bug fix: `cstag.consensus` + POS does not consider CIGAR's softclips, so removed `extract_softclips` and `calculate_read_starts` function

--- tests/test_consensus.py
+++ tests/test_consensus.py
@@ -1,35 +1,10 @@
 from collections import deque
 from src.cstag.consensus import (
-    extract_softclips,
-    calculate_read_starts,
     split_cs_tags,
     normalize_read_lengths,
     get_consensus,
     consensus,
 )
-
-
-def test_extract_softclips():
-    # Test with a variety of CIGAR strings
-    cigar_strings = ["4S3M", "3M4S", "10M", "5S5M5S", "5M"]
-    expected_output = [4, 0, 0, 5, 0]
-
-    assert extract_softclips(cigar_strings) == expected_output
-
-    # Test with empty CIGAR string
-    assert extract_softclips([""]) == [0]
-
-    # Test with no CIGAR strings
-    assert extract_softclips([]) == []
-
-
-def test_calculate_read_starts():
-    # Test with a variety of POS and CIGAR strings
-    POS = [6, 8, 10]
-    CIGAR = ["4S3M", "3M", "3S2M"]
-    expected_output = [0 + 4, 2 + 0, 4 + 3]
-
-    assert calculate_read_starts(POS, CIGAR) == expected_output
 
 
 def test_split_cs_tags():
@@ -96,30 +71,26 @@ def test_substitution():
         "=C*gt=T",
         "=ACT+ccc=T",
     ]
-    CIGAR = ["4M", "4M", "1S3M", "3M", "3M3I1M"]
-    POS = [1, 1, 1, 2, 1]
-    assert consensus(CSTAG, CIGAR, POS) == "=AC*gt=T"
+    POS = [1, 1, 2, 2, 1]
+    assert consensus(CSTAG, POS) == "=AC*gt=T"
 
 
 def test_insertion():
     CSTAG = ["=ACGT", "=AC+ggggg=GT", "=C+ggggg=GT", "=C+ggggg=GT"]
-    CIGAR = ["4M", "2M5I1M", "1S5I3M", "1M5I1M"]
-    POS = [1, 1, 1, 2]
-    assert consensus(CSTAG, CIGAR, POS) == "=NC+ggggg=GT"
+    POS = [1, 1, 2, 2]
+    assert consensus(CSTAG, POS) == "=NC+ggggg=GT"
 
 
 def test_deletion():
     CSTAG = ["=ACGT", "=AC-ggggg=GT", "=C-ggggg=GT", "=C-ggggg=GT"]
-    CIGAR = ["4M", "2M5D1M", "1S5D3M", "1M5D1M"]
-    POS = [1, 1, 1, 2]
-    assert consensus(CSTAG, CIGAR, POS) == "=NC-ggggg=GT"
+    POS = [1, 1, 2, 2]
+    assert consensus(CSTAG, POS) == "=NC-ggggg=GT"
 
 
-def test_softclip():
+def test_positions():
     CSTAG = ["=ACGT", "=CGT", "=GT"]
-    CIGAR = ["4M", "1S3M", "2S2M"]
-    POS = [1, 1, 1]
-    assert consensus(CSTAG, CIGAR, POS) == "=NCGT"
+    POS = [1, 2, 3]
+    assert consensus(CSTAG, POS) == "=NCGT"
 
 
 def test_splicing():
@@ -129,6 +100,5 @@ def test_splicing():
         "=C~gc100ag=T",
         "=C~gc100ag=T",
     ]
-    CIGAR = ["4M", "2M100N1M", "1S100N3M", "1M100N1M"]
-    POS = [1, 1, 1, 2]
-    assert consensus(CSTAG, CIGAR, POS) == "=NC~gc100ag=T"
+    POS = [1, 1, 2, 2]
+    assert consensus(CSTAG, POS) == "=NC~gc100ag=T"
